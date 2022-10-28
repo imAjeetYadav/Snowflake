dbfd7fabde6c5c4f44cf6aed7dc15d800397325d
amustakim <amustakim@users.noreply.github.com>
2022-10-28 13:48:23 -0700

    Create SP2.sql

---
+++ SQL/Schema2/Stored Procedures/SP2.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE PROCEDURE Schema2.simple_stored_procedure_example()
+returns string not null
+language javascript
+as
+$$
+var cmd = `
+<SOME SUPER SWEET SQL STATEMENT>
+`
+var sql = snowflake.createStatement({sqlText: cmd});
+var result = sql.execute();
+return '💰';
+$$;
